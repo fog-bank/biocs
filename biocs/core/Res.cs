67f4ad32688bd13de5d989a6a32dee4b5c03250c
fog-bank <fog-bank@users.noreply.github.com>
2016-10-11 05:14:42 +0900

    Provides the class that returns string resources.

---
+++ biocs/core/Res.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace Biocs
+{
+	internal static class Res
+	{
+		private static readonly ResourceManager rm = new ResourceManager(typeof(Res));
+
+		public static string GetString(string name) => rm.GetString(name);
+
+		public static string GetString(string name, object arg0) => GetStringInternal(name, arg0);
+
+		public static string GetString(string name, object arg0, object arg1) => GetStringInternal(name, arg0, arg1);
+
+		public static string GetString(string name, object arg0, object arg1, object arg2) => GetStringInternal(name, arg0, arg1, arg2);
+
+		private static string GetStringInternal(string name, params object[] args) => string.Format(CultureInfo.CurrentCulture, GetString(name), args);
+	}
+}
