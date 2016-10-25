ea7b639aeb3a97a470f746bf27f481bc525f4db1
fog-bank <fog-bank@users.noreply.github.com>
2016-10-26 00:53:16 +0900

    Represent DNA bases (#4)
    * Define DNA symbol
    * Add a code analysis dictionary
    * Add CI configuration

--- biocs/core/Properties/AssemblyInfo.cs
+++ biocs/core/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Resources;
 [assembly: AssemblyTitle("Biocs.Core.dll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("co3366353")]
+[assembly: AssemblyProduct("BioCSharp")]
 [assembly: AssemblyCopyright("Copyright © 2016 fog-bank. Some rights reserved.")]
 [assembly: AssemblyTrademark("")]
-[assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
+[assembly: CLSCompliant(true)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,6 +25,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.11012.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.11026.0")]
 
