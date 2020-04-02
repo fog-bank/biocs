12abbe29c02822f8f4e5045641d8767c0fe10dd1
fog-bank <fog-bank@users.noreply.github.com>
2020-04-02 20:05:04 +0900

    WIP

---
+++ biocs/core/Location.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Biocs
+{
+    /// <summary>
+    /// Represents the region of the biological sequence.
+    /// </summary>
+    /// <remarks>
+    /// Compliant with [The DDBJ/ENA/GenBank Feature Table Definition](http://www.insdc.org/files/feature_table.html)
+    /// </remarks>
+    public class Location : IEquatable<Location?>, IComparable<Location?>
+    {
+        private int start;
+        private int end;
+        private LocationOperator locationOperator;
+
+        public int Start => IsSpan ? start : Elements[0].Start;
+
+        public int End => IsSpan ? end : Elements[^1].End;
+
+        public int Length { get; }
+
+        public bool IsExactStart { get; }
+
+        public bool IsExactEnd { get; }
+
+        public string? SequenceID { get; }
+
+        public IReadOnlyList<Location> Elements { get; }
+
+        public bool IsSpan => locationOperator == LocationOperator.Span;
+
+        public bool IsComplement { get; }
+
+        public bool Equals(Location? other)
+        {
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (other is null)
+                return false;
+
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as Location);
+
+        public int CompareTo(Location? other)
+        {
+            if (ReferenceEquals(this, other))
+                return 0;
+
+            if (other is null)
+                return 1;
+
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Start, End, IsComplement);
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Location Parse(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool operator ==(Location? left, Location? right)
+        {
+            return left is null ? right is null : left.Equals(right);
+        }
+
+        public static bool operator !=(Location? left, Location? right) => !(left == right);
+
+        public static bool operator <(Location? left, Location? right)
+        {
+            return left is null ? right is object : left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(Location? left, Location? right)
+        {
+            return left is null || left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(Location? left, Location? right) => right < left;
+
+        public static bool operator >=(Location? left, Location? right) => right <= left;
+    }
+
+    internal enum LocationOperator
+    {
+        Span,
+        Site,
+        Join,
+        Complement,
+        Order
+    }
+}
