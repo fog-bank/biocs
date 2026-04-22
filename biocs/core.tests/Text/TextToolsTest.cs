9129614d5aa517a5977fbc8516d90c6c96cb3899
fog-bank <fog-bank@users.noreply.github.com>
2026-04-22 18:21:14 +0900

    TextTools.AsTsv

---
+++ biocs/core.tests/Text/TextToolsTest.cs
@@ -0,0 +1,175 @@
+﻿namespace Biocs.Text;
+
+[TestClass]
+public class TextToolsTest
+{
+    [TestMethod]
+    public void AsTsvTest()
+    {
+        // case 1
+        var enumerator = string.Empty.AsSpan().AsTsv();
+        int count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.IsEmpty);
+            count++;
+        }
+        Assert.AreEqual(1, count);
+
+        // case 2
+        string value = "empty";
+        enumerator = value.AsSpan().AsTsv();
+        count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual("empty"));
+            count++;
+        }
+        Assert.AreEqual(1, count);
+
+        (var x, var y) = value.AsSpan().AsTsv();
+        Assert.IsTrue(x.SequenceEqual("empty"));
+        Assert.IsTrue(y.IsEmpty);
+
+        // case 3
+        value = "\tx\t\tyz\t";
+        enumerator = value.AsSpan().AsTsv();
+        count = 0;
+        string[] values = [string.Empty, "x", string.Empty, "yz", string.Empty];
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count]));
+            count++;
+        }
+        Assert.AreEqual(5, count);
+
+        // case 4
+        enumerator = value.AsSpan(1..^1).AsTsv();
+        count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count + 1]));
+            count++;
+        }
+        Assert.AreEqual(3, count);
+
+        (x, y, var z) = value.AsSpan(1..^1).AsTsv();
+        Assert.IsTrue(x.SequenceEqual(values[1]));
+        Assert.IsTrue(y.SequenceEqual(values[2]));
+        Assert.IsTrue(z.SequenceEqual(values[3]));
+    }
+
+    [TestMethod]
+    public void FromSeparatedValuesTest()
+    {
+        // case 1
+        var enumerator = string.Empty.AsSpan().AsSeparatedValues("\t");
+        int count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.IsEmpty);
+            count++;
+        }
+        Assert.AreEqual(1, count);
+
+        // case 2
+        string value = "empty";
+        enumerator = value.AsSpan().AsSeparatedValues("\t");
+        count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual("empty"));
+            count++;
+        }
+        Assert.AreEqual(1, count);
+
+        (var x, var y) = value.AsSpan().AsSeparatedValues("\t");
+        Assert.IsTrue(x.SequenceEqual("empty"));
+        Assert.IsTrue(y.IsEmpty);
+
+        // case 3
+        value = "\tx\t\tyz\t";
+        enumerator = value.AsSpan().AsSeparatedValues("\t");
+        count = 0;
+        string[] values = [string.Empty, "x", string.Empty, "yz", string.Empty];
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count]));
+            count++;
+        }
+        Assert.AreEqual(5, count);
+
+        // case 4
+        enumerator = value.AsSpan()[1..^1].AsSeparatedValues("\t");
+        count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count + 1]));
+            count++;
+        }
+        Assert.AreEqual(3, count);
+
+        // case 5
+        value = "chemical synaptic transmission | chloride transmembrane transport | ion transmembrane transport";
+        enumerator = value.AsSpan().AsSeparatedValues(" | ");
+        count = 0;
+        values = ["chemical synaptic transmission", "chloride transmembrane transport", "ion transmembrane transport"];
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count]));
+            count++;
+        }
+        Assert.AreEqual(3, count);
+
+        // case 6
+
+        value = " |  |  |A | ";
+        enumerator = value.AsSpan().AsSeparatedValues(" | ");
+        count = 0;
+        values = [string.Empty, string.Empty, " |A", string.Empty];
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count]));
+            count++;
+        }
+        Assert.AreEqual(4, count);
+    }
+
+    [TestMethod]
+    public void FromSeparatedValuesAnyTest()
+    {
+        string value = " \t";
+        var enumerator = value.AsSpan().AsSeparatedValuesAny(" \t");
+        int count = 0;
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.IsEmpty);
+            count++;
+        }
+        Assert.AreEqual(3, count);
+
+        value = "ID=FBsf0000411533;Name=BKN40131;Dbxref=FlyBase:FBsf0000411533;";
+        enumerator = value.AsSpan().AsSeparatedValuesAny(" =:;");
+        count = 0;
+        string[] values =
+            ["ID", "FBsf0000411533", "Name", "BKN40131", "Dbxref", "FlyBase", "FBsf0000411533", string.Empty];
+
+        foreach (var span in enumerator)
+        {
+            Assert.IsTrue(span.SequenceEqual(values[count]));
+            count++;
+        }
+        Assert.AreEqual(values.Length, count);
+    }
+}
---
+++ biocs/core/Text/TextTools.cs
@@ -0,0 +1,45 @@
+﻿namespace Biocs.Text;
+
+/// <summary>
+/// Provides utility methods to handle text data.
+/// </summary>
+public static class TextTools
+{
+    /// <summary>
+    /// Splits the specified character span into values separated by the specified separator.
+    /// </summary>
+    /// <param name="record">The span of characters to split.</param>
+    /// <param name="separator">The character used to separate values.</param>
+    /// <returns>An enumerator that iterates through the separated values.</returns>
+    /// <remarks>
+    /// Each value is represented as a slice of the original span.
+    /// This method is useful for parsing delimited text files such as CSV or TSV.
+    /// </remarks>
+    public static SeparatedValueEnumerator AsSeparatedValues(this ReadOnlySpan<char> record, char separator)
+        => new(record, separator);
+
+    /// <summary>
+    /// Splits the specified character span into values separated by the specified separator.
+    /// </summary>
+    /// <param name="record">The span of characters to split.</param>
+    /// <param name="separator">The span of characters used to separate values.</param>
+    /// <returns>An enumerator that iterates through the separated values.</returns>
+    public static SeparatedValueEnumerator AsSeparatedValues(this ReadOnlySpan<char> record, ReadOnlySpan<char> separator)
+        => new(record, separator, false);
+
+    /// <summary>
+    /// Splits the specified character span into values separated by any occurrence of the specified separator sequence.
+    /// </summary>
+    /// <param name="recod">The span of characters to split.</param>
+    /// <param name="separators">Any number of characters that may delimit the span.</param>
+    /// <returns>An enumerator that iterates through the separated values.</returns>
+    public static SeparatedValueEnumerator AsSeparatedValuesAny(this ReadOnlySpan<char> recod, ReadOnlySpan<char> separators)
+        => new(recod, separators, true);
+
+    /// <summary>
+    /// Splits the specified character span into tab-separated values.
+    /// </summary>
+    /// <param name="record">The span of characters to split, where values are separated by tab characters ('\t').</param>
+    /// <returns>An enumerator that iterates through the tab-separated values.</returns>
+    public static SeparatedValueEnumerator AsTsv(this ReadOnlySpan<char> record) => AsSeparatedValues(record, '\t');
+}
